d1f148ead7c5836d5945be891c40d9dd21dff149
ju-to-sh <088t3748@gmail.com>
2023-11-24 06:38:55 +0900

    カードコンポーネントの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { CalculationCard } from "@/components/organisms/CalculationCard";
+import { Container } from "@chakra-ui/react";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <Container maxW="600px" bg="gray.200" color="#333333" p="16px">
+      <CalculationCard src="/img/car.svg" alt="足し算の写真" />
+    </Container>
+  );
 }
---
+++ src/components/organisms/CalculationCard.tsx
@@ -0,0 +1,31 @@
+import { Card, CardBody, CardFooter, Button, Heading, Text, Stack } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  src: string;
+  alt: string;
+};
+
+export const CalculationCard = (props: Props) => {
+  const { src, alt } = { ...props };
+
+  return (
+    <Card maxW="250px" m="0 auto">
+      <CardBody m="0 auto">
+        <Stack>
+          <Image src={src} width={150} height={150} alt={alt} />
+          <Heading size="md">かけざん</Heading>
+          <Text>小学校3年生レベル</Text>
+        </Stack>
+      </CardBody>
+      <CardFooter m="0 auto">
+        <Link href="/">
+          <Button variant="solid" colorScheme="blue">
+            始める
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
