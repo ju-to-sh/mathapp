643b611ed26615c7f0e06049e6f89bed6e03f66f
ju-to-sh <088t3748@gmail.com>
2023-11-26 07:23:49 +0900

    かけざんページへ遷移できるように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Container } from "@chakra-ui/react";
 const Home: NextPage = () => {
   return (
     <Container maxW="600px" bg="gray.200" color="#333333" p="16px">
-      <CalculationCard src="/img/car.svg" alt="足し算の写真" />
+      <CalculationCard src="/img/car.svg" alt="足し算の写真" href="/multiplication" />
     </Container>
   );
 };
